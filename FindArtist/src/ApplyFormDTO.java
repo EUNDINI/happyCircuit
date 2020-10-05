0a0da2da36b3bbb0e7ad7fd83e71a2d8266f73fc
LAPTOP-D5OGQ982\EUNDINI <lee08236@naver.com>
2020-10-05 21:48:17 +0900

    FindArtist 2 commit

--- FindArtist/src/ApplyFormDTO.java
+++ FindArtist/src/ApplyFormDTO.java
@@ -1,4 +1,5 @@
 import java.io.File;
+import java.text.SimpleDateFormat;
 
 public class ApplyFormDTO {
 	
@@ -6,8 +7,12 @@ public class ApplyFormDTO {
 	private String userName; // 사용자 이름
 	private String userNickname; // 사용자 닉네임 
 	// 이름 또는 닉네임 둘 중 하나만 남겨도 될 거 같음....!
+	//위에 3개는 fk(외래키)로 받아올 거
 	
+	private long applyNumber; // 신청폼 번호, pk(기본키)
 	private String applyTitle; // 신청폼 제목
+	private SimpleDateFormat applyDate; // 신청폼을 작성한 날짜
+	private long applyviews; // 신청폼 조회수
 	private String applyCategory; // 신청폼 카테고리
 	private String applyContent; // 신청폼 내용
 	private File applyAttachment; // 신청폼에 첨부할 수 있는 파일
@@ -15,17 +20,19 @@ public class ApplyFormDTO {
 	// 필드 추가 될만한 거 있으면 여기에 더 넣으면 될듯..?
 	
 	// 일단 생성자는 모든 필드를 포함한 거 하나만 만듬, 필드 수정 시에 생성자도 수정할 필요가 있음
-	public ApplyFormDTO(String userId, String userName, String userNickname, String applyTitle, String applyCategory,
-			String applyContent, File applyAttachment) {
-		super();
+	ApplyFormDTO(String userId, String userName, String userNickname, long applyNumber, String applyTitle,
+			SimpleDateFormat applyDate, long applyviews, String applyCategory, String applyContent,
+			File applyAttachment) {
 		this.userId = userId;
 		this.userName = userName;
 		this.userNickname = userNickname;
+		this.applyNumber = applyNumber;
 		this.applyTitle = applyTitle;
+		this.applyDate = applyDate;
+		this.applyviews = applyviews;
 		this.applyCategory = applyCategory;
 		this.applyContent = applyContent;
 		this.applyAttachment = applyAttachment;
-		
 	}
 	
 	public String getUserId() {
