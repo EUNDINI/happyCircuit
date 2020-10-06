a3f976994f0d3108a9d98ae1f9cae70996ea81b5
aaa <janu01ary@gmail.com>
2020-10-06 23:17:09 +0900

    주석 추가, 시간 format 추가

--- src/model/DM.java
+++ src/model/DM.java
@@ -1,13 +1,13 @@
-package happy;
+package model;
 
 import java.util.ArrayList;
 
-public class DMDTO {
+public class DM {
 	
-	private long _id;
-	private ArrayList<String> userIdList;
+	private long _id;		//DM방의 id (채팅방같이)
+	private ArrayList<String> userIdList;	//DM방에 속한 user의 id 리스트
 
-	public DMDTO(long _id, ArrayList<String> userIdList) {
+	public DM(long _id, ArrayList<String> userIdList) {
 		this._id = _id;
 		this.userIdList = userIdList;
 	}
