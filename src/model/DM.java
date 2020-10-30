5d60a87f6d71647017a87229f6db816c562a8585
aaa <janu01ary@gmail.com>
2020-10-30 11:49:06 +0900

    dto 수정, 추천 jsp 추가

--- src/model/DM.java
+++ src/model/DM.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 public class DM {
 	
 	private long _id;		//DM방의 id (채팅방같이)
+	//roomId 이런 식으로 해도 될 듯? _id 안 쓴다면
 	private ArrayList<String> userIdList;	//DM방에 속한 user의 id 리스트
+	//그냥 ArrayList<User>해도 될 것 같기도 하고...
 
 	public DM(long _id, ArrayList<String> userIdList) {
 		this._id = _id;
