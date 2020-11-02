0164fbdb1c610be4fc06d3a70ea116d7d4fa6502
aaa <janu01ary@gmail.com>
2020-11-02 13:29:46 +0900

    dto 수정(er다이어그램 하면서 수정한 거 바탕으로)

--- src/model/Message.java
+++ src/model/Message.java
@@ -4,26 +4,43 @@ import java.text.SimpleDateFormat;
 
 public class Message {
 	
-	private long _id;			//메시지의 id
-	//_id 안 쓴다면 msg_id나 msgId 이런식 
-	private long roomId;		//메시지가 포함된 DM방의 id
+	private long msgId;			//메시지의 id
+	private Artist artistId;	//보내는 사람 id
+	private long dmId;			//메시지가 포함된 DM방의 id
 	private String message;		//메시지 내용
-	private String senderId;	//보내는 사람 id string id로 받아올지 long _id로 받아올지
-	//아니면 그냥 User sender 이렇게 받아와도...
-	private SimpleDateFormat time;	//메시지를 보낸 시간
+	private SimpleDateFormat sentTime;	//메시지를 보낸 시간
 	
-	public Message(String message, String senderId, SimpleDateFormat time) {
+	public Message(long msgId, Artist artistId, long dmId, String message, SimpleDateFormat sentTime) {
+		super();
+		this.msgId = msgId;
+		this.artistId = artistId;
+		this.dmId = dmId;
 		this.message = message;
-		this.senderId = senderId;
-		this.time = time;
-	}
-	
-	public long get_id() {
-		return _id;
+		this.sentTime = sentTime;
 	}
 
-	public void set_id(long _id) {
-		this._id = _id;
+	public long getMsgId() {
+		return msgId;
+	}
+
+	public void setMsgId(long msgId) {
+		this.msgId = msgId;
+	}
+
+	public Artist getArtistId() {
+		return artistId;
+	}
+
+	public void setArtistId(Artist artistId) {
+		this.artistId = artistId;
+	}
+
+	public long getDmId() {
+		return dmId;
+	}
+
+	public void setDmId(long dmId) {
+		this.dmId = dmId;
 	}
 
 	public String getMessage() {
@@ -34,19 +51,12 @@ public class Message {
 		this.message = message;
 	}
 
-	public String getSender() {
-		return senderId;
+	public SimpleDateFormat getSentTime() {
+		return sentTime;
 	}
 
-	public void setSender(String senderId) {
-		this.senderId = senderId;
+	public void setSentTime(SimpleDateFormat sentTime) {
+		this.sentTime = sentTime;
 	}
-
-	public SimpleDateFormat getTime() {
-		return time;
-	}
-
-	public void setTime(SimpleDateFormat time) {
-		this.time = time;
-	}
+	
 }
